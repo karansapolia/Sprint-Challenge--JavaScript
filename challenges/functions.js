6f7a072eb6aaf292121dba154e1b32677831005c
Karan Sapolia <sapoliakaran@gmail.com>
2019-12-20 22:02:44 +0530

    feat: Answer readme questions

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,48 +1,48 @@
-// ==== Callbacks ====  
+// // ==== Callbacks ====  
 
-/* Step 1: Create a higher-order function
-  * Create a higher-order function named consume with 3 parameters: a, b and cb
-  * The first two parameters can take any argument (we can pass any value as argument)
-  * The last parameter accepts a callback
-  * The consume function should return the invocation of cb, passing a and b into cb as arguments
-*/
+// /* Step 1: Create a higher-order function
+//   * Create a higher-order function named consume with 3 parameters: a, b and cb
+//   * The first two parameters can take any argument (we can pass any value as argument)
+//   * The last parameter accepts a callback
+//   * The consume function should return the invocation of cb, passing a and b into cb as arguments
+// */
 
-function consume(a, b, cb) {
-  return cb(a, b);
-}
+// function consume(a, b, cb) {
+//   return cb(a, b);
+// }
 
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+// /* Step 2: Create several functions to callback with consume();
+//   * Create a function named add that returns the sum of two numbers
+//   * Create a function named multiply that returns the product of two numbers 
+//   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+// */
 
-const add = (a, b) => a + b;
-const multiply = (a, b) => a * b;
-const greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`;
+// const add = (a, b) => a + b;
+// const multiply = (a, b) => a * b;
+// const greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`;
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-console.log(consume(2, 2, add)); // 4
-console.log(consume(10, 16, multiply)); // 160
-console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-
-
-// ==== Closures ==== 
-
-// Explain in your own words why nestedfunction can access the variable internal.
-
-// Explanation: The const internal and nestedFunction() are part of one closure. All functions in JS are bound together with their lexical environment, which includes const internal in this case with nestedFunction().
+// /* Step 3: Check your work by un-commenting the following calls to consume(): */
+// console.log(consume(2, 2, add)); // 4
+// console.log(consume(10, 16, multiply)); // 160
+// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
-const external = "I'm outside the function";
+// // ==== Closures ==== 
 
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+// // Explain in your own words why nestedfunction can access the variable internal.
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+// // Explanation: The const internal and nestedFunction() are part of one closure. All functions in JS are bound together with their lexical environment, which includes const internal in this case with nestedFunction().
+
+
+// const external = "I'm outside the function";
+
+// function myFunction() {
+//   console.log(external);
+//   const internal = "Hello! I'm inside myFunction!";
+
+//   function nestedFunction() {
+//     console.log(internal);
+//   };
+//   nestedFunction();
+// }
+// myFunction();
